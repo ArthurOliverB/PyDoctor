38731d9c513be230facad027d7e7997133c701da
Arthur Oliveira <arthuroliveirabandeira@hotmail.com>
2020-03-02 13:12:06 -0300

    adicionando a rota usuario

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/usuarios', (req, res, next) => {
 	res.send("Hello!")
 })
 
+app.get('/usuarios/:id', (req, res, next) => {
+	res.send("João")
+})
 
 app.listen(3000, () => {
 	console.log("Rodando na porta 3000")
